12919775338311afc4ba53c128ca1ffc2dcd9114
Joshua Samontanez <jsamontanez@knights.ucf.edu>
2023-03-26 18:07:15 -0400

    Added pokefi-mobile

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
+import React, { useEffect } from 'react';
+import { WebView } from 'react-native-webview';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  useEffect(() => {
+    async function init() {
+      // Prevent splash screen from autohiding
+      await SplashScreen.preventAutoHideAsync();
+    }
+    init();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <WebView
+        source={{ uri: 'https://pokefi.herokuapp.com/' }}
+        sharedCookiesEnabled={true}
+        thirdPartyCookiesEnabled={true}
+        onLoad={() => SplashScreen.hideAsync()}
+      />
+      <StatusBar translucent={false} backgroundColor='#555456' style='light' />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
